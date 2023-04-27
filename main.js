360f3287fd557f0d88a2b0f5a0e54884e4f8cf24
1Niaz <niazbekturov353@gmail.com>
2023-04-27 18:54:23 +0600

    message

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-let btn = document.querySelector('.btn-start')
+const inputs = document.querySelectorAll('.hang-input');
+const gallowEls = document.querySelectorAll('.hang-indicator');
+let indexGallowEl = 0;
+let trueAnswers = 0;
+let falseAnswers = 0;
+const word = {
+  word: 'привет',
+  help1: '',
+  help2: '',
+  help3: ''
+};
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('change', function (e) {
+    if (inputs[i] === word.word[i]) {
+      trueAnswers++;
+      // удалить элемент inputs[i] из inputs, чтобы больше его не слушать
+    } else {
+      falseAnswers++;
+      gallowEls[indexGallowEl].classList.add('active');
+      indexGallowEl++;
+    }
+    if (falseAnswers >= 5) {
+      // проигрыш
+    }
+    if (trueAnswers == 5) {
+      // победа
+    }
+  });
+}
