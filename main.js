159bc25e141db3db4f4b46ba8159a044dddec87e
1Niaz <niazbekturov353@gmail.com>
2023-04-30 18:42:29 +0600

    message

--- main.js
+++ main.js
@@ -3,28 +3,53 @@ const gallowEls = document.querySelectorAll('.hang-indicator');
 let indexGallowEl = 0;
 let trueAnswers = 0;
 let falseAnswers = 0;
-const word = {
-  word: 'привет',
-  help1: '',
-  help2: '',
-  help3: ''
-};
+
+const words = [
+  {
+    word: 'банка',
+    help1: '',
+    help2: '',
+    help3: ''
+  },
+  {
+    word: 'баqка',
+    help1: '',
+    help2: '',
+    help3: ''
+  },
+  {
+    word: 'баfка',
+    help1: '',
+    help2: '',
+    help3: ''
+  }
+];
+
+const indexOfWord = Math.round(Math.random() * (words.length - 1))
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('change', function (e) {
-    if (inputs[i] === word.word[i]) {
-      trueAnswers++;
-      // удалить элемент inputs[i] из inputs, чтобы больше его не слушать
-    } else {
-      falseAnswers++;
-      gallowEls[indexGallowEl].classList.add('active');
-      indexGallowEl++;
-    }
-    if (falseAnswers >= 5) {
-      // проигрыш
-    }
-    if (trueAnswers == 5) {
-      // победа
-    }
-  });
+    inputs[i].addEventListener('input', function () {
+      if (inputs[i].value.length == 1) {
+        
+        if (inputs[i].value != words[indexOfWord].word[i]) {
+          
+          
+          // если не угадали букву
+          inputs[i].style.backgroundColor = "#ff0000";
+          
+          gallowEls[indexGallowEl].classList.add('active');
+          indexGallowEl++;
+          
+          if (indexGallowEl >= 5) {
+            console.log('You lose!')
+            // перевод на следующий экран
+          }
+          
+        } else {
+          // правильно введенная буква
+          inputs[i].style.backgroundColor = "#00ff00";
+        }
+        
+      }
+    });
 }
