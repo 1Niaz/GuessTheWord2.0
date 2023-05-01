67b8ec5f9e96d29ace95b9360e6a386e65a36876
1Niaz <niazbekturov353@gmail.com>
2023-05-01 21:59:57 +0600

    message

--- main.js
+++ main.js
@@ -30,52 +30,36 @@ startBtn.addEventListener('click', function () {
 
 });
 
+help.addEventListener('click', function(){
+  alert(words[indexOfWord].help);
+  gallowEls[indexGallowEl].classList.add('active');
+  indexGallowEl++;
+});
 const indexOfWord = Math.round(Math.random() * (words.length - 1))
 
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('input', function () {
       if (inputs[i].value.length == 1) {
-        
+
         if (inputs[i].value != words[indexOfWord].word[i]) {
-          
-          
+
+
           // если не угадали букву
           inputs[i].style.backgroundColor = "#be0808";
-          
+
           gallowEls[indexGallowEl].classList.add('active');
           indexGallowEl++;
-          
+
           if (indexGallowEl >= 5) {
             console.log('You lose!')
             // перевод на следующий экран
           }
-          
+
         } else {
           // правильно введенная буква
           inputs[i].style.backgroundColor = "#12a112";
         }
-        
+
       }
     });
 }
-
-for (let i = 0; i < 1; i++){
-  help.addEventListener('click', function(){
-    gallowEls[i].classList.add('active');
-    alert(words[indexOfWord].help);
-
-  });
-}
-
-/*for (let i = 1; i < 2; i++){
-      help.addEventListener('click', function(){
-        gallowEls[i].classList.add('active');
-
-        for (let i = 2; i < 3; i++){
-          help.addEventListener('click', function(){
-            gallowEls[i].classList.add('active');
-            
-          });
-        }
-      });
-    }*/
